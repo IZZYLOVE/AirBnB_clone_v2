9e5555a269661b4e2d32bc5886b3eb25ab6bd549
solokool66 <solokool66@gmail.com>
2023-06-21 07:14:45 -0700

    static web do deploy corrected

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -26,17 +26,16 @@ def do_pack():
         return None
     return file
 
-
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
     Args:
-        archive_path (str): The path of the archive to distribute.
+    archive_path (str): The path of the archive to distribute.
     Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
+    If the file doesn't exist at archive_path or an error occurs - False.
+    Otherwise - True.
     """
     if os.path.isfile(archive_path) is False:
-        return False
+    return False
     file = archive_path.split("/")[-1]
     name = file.split(".")[0]
 
